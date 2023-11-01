4d6034da0b094ff339a9a03cf99bbd1e35430c9e
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-11-01 15:46:19 -0300

    se termino el ejercicio 2

--- js/2-Ejercicio2.js
+++ js/2-Ejercicio2.js
@@ -16,7 +16,14 @@ document.write(`<p>El arreglo contiene ${arrayDeCiudades.length} elementos</p>`)
 document.write('<ul>');
 document.write(`<li>Elemento 1er posición: ${arrayDeCiudades[0]}</li>`);
 document.write(`<li>Elemento 3er posición: ${arrayDeCiudades[2]}</li>`);
-document.write(`<li>Elemento ultima posición: ${arrayDeCiudades.length-1}</li>`);
+document.write(`<li>Elemento ultima posición: ${arrayDeCiudades[arrayDeCiudades.length-1]}</li>`);
+arrayDeCiudades.push('Paris, Francia');
+document.write(`<li>Elemento ultima posición: ${arrayDeCiudades[arrayDeCiudades.length-1]}</li>`);
 document.write('</ul>');
 
-document.write(arrayDeCiudades);
+document.write('<h2>Arreglo de Ciudades</h2>')
+document.write('<ul>');
+for(let i = 0; i < arrayDeCiudades.length; i++){
+    document.write(`<li>${arrayDeCiudades[i]}</li>`);
+}
+document.write('</ul>');
