0961c699efcdd1f4c050ef46c856a389429adf7d
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-11-01 15:40:07 -0300

    se creo la primer estructura para mostrar los elementos en diferentes posiciones

--- js/2-Ejercicio2.js
+++ js/2-Ejercicio2.js
@@ -11,4 +11,12 @@ do{
 
 }while(confirm("Desea ingresar otra ciudad?"))
 
+document.write(`<p>El arreglo contiene ${arrayDeCiudades.length} elementos</p>`);
+
+document.write('<ul>');
+document.write(`<li>Elemento 1er posición: ${arrayDeCiudades[0]}</li>`);
+document.write(`<li>Elemento 3er posición: ${arrayDeCiudades[2]}</li>`);
+document.write(`<li>Elemento ultima posición: ${arrayDeCiudades.length-1}</li>`);
+document.write('</ul>');
+
 document.write(arrayDeCiudades);
