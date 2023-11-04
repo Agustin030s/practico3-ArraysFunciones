7d12d87249dec7b7143518fd2d071b16df9a3501
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-11-04 16:12:52 -0300

    se realizo la tabla para mostrar los resultados de las multiplicaciones

--- js/7-Ejercicio7.js
+++ js/7-Ejercicio7.js
@@ -2,14 +2,33 @@
 Escriba un script que muestre la tabla de multiplicar de un número ingresado por pantalla, la creación de la tabla debe ser realizada con una función y mostrar solo los resultados del 1 al 10 del número elegido por el usuario.
 */
 
-const numeroIngresado = parseInt(prompt("Ingrese un número para saber su tabla de multiplicar"));
+const numeroIngresado = parseInt(
+  prompt("Ingrese un número para saber su tabla de multiplicar")
+);
 
-const tablaDeMultiplicar = numero =>{
-    let resultado = undefined;
-    for(let i = 1; i <= 10; i++){
-        resultado = numero*i;
-        console.log(resultado);
-    }
-}
+const tablaDeMultiplicar = (numero) => {
+  let resultado = undefined;
+  for (let i = 1; i <= 10; i++) {
+    resultado = numero * i;
+    document.write("<tr>");
+    document.write(`<td>${numero} x ${i}</td>`);
+    document.write(`<td>${resultado}</td>`);
+    document.write("</tr>");
+    console.log(resultado);
+  }
+};
+
+document.write(`<h2>Tabla de Multiplicar del ${numeroIngresado}</h2>`);
+
+document.write('<table class="m-5">');
+document.write(`<thead>
+<tr>
+    <th>Multiplicación</th>
+    <th>Resultado</th>
+</tr>
+</thead>`);
 
 tablaDeMultiplicar(numeroIngresado);
+
+document.write("</tbody>");
+document.write("</table>");
