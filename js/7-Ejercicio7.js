d180801bf7e4be7b9b8b56878ad98a999eaa947e
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-11-04 12:45:21 -0300

    se realizo la logica del ejercicio 7

--- js/7-Ejercicio7.js
+++ js/7-Ejercicio7.js
@@ -1,3 +1,15 @@
 /*
 Escriba un script que muestre la tabla de multiplicar de un número ingresado por pantalla, la creación de la tabla debe ser realizada con una función y mostrar solo los resultados del 1 al 10 del número elegido por el usuario.
 */
+
+const numeroIngresado = parseInt(prompt("Ingrese un número para saber su tabla de multiplicar"));
+
+const tablaDeMultiplicar = numero =>{
+    let resultado = undefined;
+    for(let i = 1; i <= 10; i++){
+        resultado = numero*i;
+        console.log(resultado);
+    }
+}
+
+tablaDeMultiplicar(numeroIngresado);
