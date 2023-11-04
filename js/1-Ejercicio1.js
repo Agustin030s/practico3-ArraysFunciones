f9a784f097d085511160b3b92766c440536adcba
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-11-04 12:03:36 -0300

    se agrego los enunciados en el ejercicio 1 y 2

--- js/1-Ejercicio1.js
+++ js/1-Ejercicio1.js
@@ -1,3 +1,7 @@
+/*
+Crear un array llamado meses y que almacene el nombre de los doce meses del año. Mostrar por pantalla en forma de lista los doce nombres del arreglo.
+*/
+
 const meses = ['Enero', 'Febrero', 'Marzo','Abril', 'Mayo', 'Junio', 
 'Julio', 'Agosto', 'Septiemnbre', 'Octubre', 'Noviembre', 'Diciembre'];
 
--- js/2-Ejercicio2.js
+++ js/2-Ejercicio2.js
@@ -1,3 +1,13 @@
+/*
+Crear un script que solicite al usuario mediante un prompt el nombre de ciudades y almacenarlas en un arreglo, cuando el usuario selecciona cancelar se debe mostrar el arreglo generado, luego realizar las siguientes acciones:
+
+-Mostrar la longitud del arreglo.
+-Mostrar en el documento web los ítems de las posiciones primera, tercera y última.
+-Añade en última posición la ciudad de París.
+-Escribe por pantalla el elemento que ocupa la segunda posición.
+-Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.
+*/
+
 const arrayDeCiudades = [];
 
 do{
@@ -15,6 +25,9 @@ document.write(`<p>El arreglo contiene ${arrayDeCiudades.length} elementos</p>`)
 
 document.write('<ul>');
 document.write(`<li>Elemento 1er posición: ${arrayDeCiudades[0]}</li>`);
+document.write(`<li>Elemento 2da posición: ${arrayDeCiudades[1]}</li>`);
+arrayDeCiudades.splice(1, 0, "Buenos Aires Argentina");
+document.write(`<li>Elemento 2da posición: ${arrayDeCiudades[1]} (Se reemplazo Barcelona)</li>`);
 document.write(`<li>Elemento 3er posición: ${arrayDeCiudades[2]}</li>`);
 document.write(`<li>Elemento ultima posición: ${arrayDeCiudades[arrayDeCiudades.length-1]}</li>`);
 arrayDeCiudades.push('Paris, Francia');
