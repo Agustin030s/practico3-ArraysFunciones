709063dbe2856c51c828bf03e1665efb4537ac17
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-11-04 11:59:18 -0300

    se agrego el enunciado en el ejercicio 4 y 5

--- js/4-Ejercicio4.js
+++ js/4-Ejercicio4.js
@@ -1,3 +1,7 @@
+/*
+Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto que indica si el número es par o impar. Mostrar por pantalla el resultado devuelto por la función.
+*/
+
 const EsParOImpar = numeroEntero => {
     if(numeroEntero % 2 === 0){
         document.write(`<h2>El número ${numeroEntero} es Par</h2>`);
